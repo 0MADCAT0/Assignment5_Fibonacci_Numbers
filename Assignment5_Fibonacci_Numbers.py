576ae5bbc93479acbc7455847184e7608b667c8c
MADCAT <orioncloud2020@gmail.com>
2020-07-11 15:21:14 +0300

    first commit

---
+++ Assignment5_Fibonacci_Numbers.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 11 15:04:08 2020
+
+@author: MADCAT
+"""
+
+
+def fibonacci(num):
+    ara = [1]
+    i = num
+    y=1
+    x=0
+    c=0
+    while i > 0:
+        c=x+y
+        ara.append(c)
+        x, y = y, c
+        i -=1
+        if c == num:
+            break
+    return  print(ara) 
+
+num = 55
+fibonacci(num)
---
+++ README.md
@@ -0,0 +1,2 @@
+in this assingment we will get a list of fibonacci number till 55 and 
+we asume that user in put is an integer.  
